4c67d195e501e41a9b3537c553c90fc211c0118c
Taiwo Adedayo {{ Dee - Why }} <66147046+Adedayo100-ux@users.noreply.github.com>
2022-07-08 12:26:40 -0400

    Add files via upload

---
+++ helloworld.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# I Adedayo Taiwo, 000830512 clarify that this is my original work and I have not copied it from anywhere or allowed anyone else to copy from it.
+
+echo "Hello World!"
+
