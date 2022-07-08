f2d6aa154560792ee05fb81f24bb9c6fdab21b9b
Taiwo Adedayo {{ Dee - Why }} <66147046+Adedayo100-ux@users.noreply.github.com>
2022-07-08 12:33:38 -0400

    Add files via upload

---
+++ wrapper.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# I Adedayo Taiwo, 000830512, clarify that this is my original work and that I have not copied it from anywhere or let anyone copy from it.
+
+cd /opt/Mohawk/bin
+
+./tws -p 5577 -s 4 -z
+
