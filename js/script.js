190fb95463ba85a66ded3bb32f18aacd2307b820
Adedayo100-ux <adedayoemmanuelt@gmail.com>
2022-07-21 08:06:54 -0400

    modal up

--- js/script.js
+++ js/script.js
@@ -29,18 +29,54 @@ function toggleSearchBar(){
 
 // Open Modal
 var modal = document.getElementById("modalWindow");
-
-function openModal(){
-    modal.classList.add("modal-open");
+var modalCarousel = document.getElementById("modalCarousel");
+// var signInForm = document.getElementById("signInForm");
+// var signUpForm = document.getElementById("signUpForm");
+function openModal(method){
     if (modal.classList.contains("modal-closed")) {
-        modal.classList.replace("modal-closed", "modal-open")
+        modal.classList.replace("modal-closed", "modal-open");
     }
+    else{
+        modal.classList.add("modal-open");      
+    }
+    switch (method) {
+        case 'signin':
+            if(modalCarousel.classList.contains("display-signup")) {
+                modalCarousel.classList.replace("display-signup", "display-signin")
+            }
+            else{
+                modalCarousel.classList.add("display-signin");
+            }
+            break;
+        case 'signup':
+            if(modalCarousel.classList.contains("display-signin")) {
+                modalCarousel.classList.replace("display-signin", "display-signup")
+            }
+            else{
+                modalCarousel.classList.add("display-signup");
+            }
+            break;
+        case 'forgotpassword':
+            if(modalCarousel.classList.contains("display-signin")) {
+                modalCarousel.classList.replace("display-signin", "display-signup")
+            }
+            else{
+                modalCarousel.classList.add("display-signup");
+            }
+            break;
+        default:
+            break;
+    }
+    modalCarousel.classList.add("display-"+method);
+    console.log("display-"+method)
 }
 // Close Modal
 
 function closeModal() {  
-    modal.classList.add("modal-closed");
     if (modal.classList.contains("modal-open")) {
-        modal.classList.replace("modal-open", "modal-closed")
+        modal.classList.replace("modal-open", "modal-closed");
+    }
+    else{
+        modal.classList.add("modal-closed");
     }
 }
